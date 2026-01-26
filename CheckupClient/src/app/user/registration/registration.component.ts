13619a980c1ac65bb004f9800a9e934dc317daf7
macxolelouzaar <macxolelouzaar@gmail.com>
2026-01-26 21:28:22 +0200

    validation for register

--- CheckupClient/src/app/user/registration/registration.component.ts
+++ CheckupClient/src/app/user/registration/registration.component.ts
@@ -19,7 +19,7 @@ passwordMatchValidators(form: FormGroup) {
   constructor(public formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
       firstName: ['', Validators.required],
-      lastName: [''],
+      lastName: ['',Validators.required],
       email: ['',[Validators.required, Validators.email]],
       password: ['',[Validators.required, Validators.minLength(6),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)]],
       confirmPassword: [''],
